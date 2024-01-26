cf13e3ec76fef935e31dd80c92455d6ab8244ee7
ergouHZ <140789367+ergouHZ@users.noreply.github.com>
2024-01-26 14:52:02 +0000

    云函数搭建成功
    权限更改之后正常

--- miniProgram240119/cloud/login/index.js
+++ miniProgram240119/cloud/login/index.js
@@ -1,10 +1,10 @@
 // 云函数入口文件
-/* const cloud = require('wx-server-sdk')
+const cloud = require('wx-server-sdk')
 
 cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
 
 // 云函数入口函数
-exports.main = async (event) => {
+exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
 
   return {
@@ -12,27 +12,5 @@ exports.main = async (event) => {
     openid: wxContext.OPENID,
     appid: wxContext.APPID,
     unionid: wxContext.UNIONID,
-  }
-} */
-
-
-
-
-const cloud = require('wx-server-sdk')
-cloud.init({
-  env: cloud.DYNAMIC_CURRENT_ENV
-})
-
-exports.main = async (event) => {
-  const { ENV, OPENID, APPID } = cloud.getWXContext()
-
-  // 如果云函数所在环境为 abc，则下面的调用就会请求到 abc 环境的数据库
-  const dbResult = await cloud.database().collection('test').get()
-
-  return {
-    dbResult,
-    ENV,
-    OPENID,
-    APPID,
   }
 }
--- miniProgram240119/pages/testPage/testPage.js
+++ miniProgram240119/pages/testPage/testPage.js
@@ -17,45 +17,45 @@ Page({
   onLoad() {
     console.log('11');
 
-    /*     const todos = db.collection('testA');
-        const todo = db.collection('todos').doc('test111'); */
+    const todos = db.collection('testA');
+    const todo = db.collection('todos').doc('test111');
 
-    db.collection("testA").add({
-      // data 字段表示需新增的 JSON 数据
-      data: {
-        _id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
-        description: "learn cloud database",
-        due: new Date("2018-09-01"),
-        tags: [
-          "cloud",
-          "database"
-        ],
-        // 为待办事项添加一个地理位置（113°E，23°N）
-        location: new db.Geo.Point(113, 23),
-        done: false
-      },
-      success: function (res) {
-        // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
-        console.log(res)
-      }
-    })
+    /*     db.collection("testA").add({
+          // data 字段表示需新增的 JSON 数据
+          data: {
+            _id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
+            description: "learn cloud database",
+            due: new Date("2018-09-01"),
+            tags: [
+              "cloud",
+              "database"
+            ],
+            // 为待办事项添加一个地理位置（113°E，23°N）
+            location: new db.Geo.Point(113, 23),
+            done: false
+          },
+          success: function (res) {
+            // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+            console.log(res)
+          }
+        }) */
 
 
 
     // 这里进行数据库的查询操作
-    /*     todos.doc('test111').get({
-          success: (res) => {
-            //res.data 包含该记录的数据
-            console.log(res.data);
-            //如果你想要在页面上显示这些数据，你需要设置 data 属性
-            this.setData({
-              todo: res.data
-            });
-          },
-          fail: (err) => {
-            console.error(err);
-          }
-        }); */
+    todos.doc('test111').get({
+      success: (res) => {
+        //res.data 包含该记录的数据
+        console.log(res.data);
+        //如果你想要在页面上显示这些数据，你需要设置 data 属性
+        this.setData({
+          todo: res.data
+        });
+      },
+      fail: (err) => {
+        console.error(err);
+      }
+    });
 
 
     /*     通过云路径id获取文件url方法
