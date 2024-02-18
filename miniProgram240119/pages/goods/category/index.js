dea715f2275cf952403f7b0a1efba4778a143e9c
ergouHZ <140789367+ergouHZ@users.noreply.github.com>
2024-02-18 23:05:07 +0000

    Update index.js
    server change
    using local file for testing

--- miniProgram240119/pages/goods/category/index.js
+++ miniProgram240119/pages/goods/category/index.js
@@ -25,11 +25,26 @@ Page({
     this.init();
   },
 
+
   //从数据库中获取分类定义
   getCategoryListReal() {
-    return db.collection('category').doc('1fe899fa65ba9c3b000000364501012c').get().then(res => {
+    /* return db.collection('category').doc('1fe899fa65ba9c3b000000364501012c').get().then(res => {
       return res.data.categories;
 
+    }); */
+
+    return new Promise((resolve, reject) => {
+      wx.getFileSystemManager().readFile({
+        filePath: `${wx.env.USER_DATA_PATH}/category.json`, // json文件位置
+        encoding: 'utf8',
+        success(res) {
+          const data = JSON.parse(res.data);
+          resolve(data.categories);
+        },
+        fail(err) {
+          reject(err);
+        }
+      });
     });
   }
 
