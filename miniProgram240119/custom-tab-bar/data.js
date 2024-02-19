184e67a98f3deae71e8bb9b4ab99fa22d4cc21c8
ergouHZ <140789367+ergouHZ@users.noreply.github.com>
2024-02-19 00:02:52 +0000

    tabbar

--- miniProgram240119/custom-tab-bar/data.js
+++ miniProgram240119/custom-tab-bar/data.js
@@ -1,22 +1,21 @@
 export default [
   {
-    icon: 'home',
+
     text: '首页',
     url: 'pages/home/home',
   },
   {
-    icon: 'houses-1',
+
     text: '租房',
-    url: 'pages/testPage/testPage',
+    url: 'pages/rent/rent',
   },
   {
-    icon: 'chat-message',
+
     text: '消息',
     url: 'pages/cart/index',
   },
 
   {
-    icon: 'user',
     text: '个人',
     url: 'pages/usercenter/index',
   },
